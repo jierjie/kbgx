92ac8fe4b86b5ca59b82086245955d4a4ee8f170
cjjcode <972137249@qq.com>
2023-07-17 21:25:21 +0800

    add 用户协议

--- src/pages/register.vue
+++ src/pages/register.vue
@@ -61,6 +61,44 @@
           </li>
         </ul>
         <p @click='register'>立即注册</p>
+        <div class='agreement'>
+          <i v-if='!isCheck' @click='agreementHandler'></i>
+          <!-- <img v-if='!isCheck' @click='agreementHandler' src="@/assets/img/check.png" alt=""> -->
+          <img v-else @click='agreementHandler' src="@/assets/img/check-square.png" alt="">
+          <span @click='agreementView'>阅读并接受《库博用户协议》</span>
+        </div>
+      </div>
+       <div class='three' v-show='isActive===3'>
+        <div>
+          <img v-if='!agreementList[0]' @click='agreementListHandler(0)' src="@/assets/img/check.png" alt="">
+          <img v-else @click='agreementListHandler(0)' src="@/assets/img/check-square.png" alt="">
+          <p>本人兹同意持续接收与库博光学（关于数据控制方的完整名称和地址，请参阅库博光学的《隐私政策》）（以下简称为“库博光学”、“我们”或“我们的”）产品和服务（包括补充产品、促销与优惠、销售区域代表、新产品/服务/优惠等）有关的新闻、优惠信息和资讯，以及参加库博光学产品、服务和教育资料相关调查的邀请，且库博光学可以收集、使用、披露和向海外传输本人在下方提供的个人信息，用于依据《隐私政策》的规定进行市场活动的目的。</p>
+        </div>
+        <div>本人同意通过下列通信渠道接收上述信息：</div>
+        <div>
+          <img v-if='!agreementList[1]' @click='agreementListHandler(1)' src="@/assets/img/check.png" alt="">
+          <img v-else @click='agreementListHandler(1)' src="@/assets/img/check-square.png" alt="">
+          <p>电子邮箱</p>
+        </div>
+        <div>
+          <img v-if='!agreementList[2]' @click='agreementListHandler(2)' src="@/assets/img/check.png" alt="">
+          <img v-else @click='agreementListHandler(2)' src="@/assets/img/check-square.png" alt="">
+          <p>电话</p>
+        </div>
+        <div>
+          <img v-if='!agreementList[3]' @click='agreementListHandler(3)' src="@/assets/img/check.png" alt="">
+          <img v-else @click='agreementListHandler(3)' src="@/assets/img/check-square.png" alt="">
+          <p>短信</p>
+        </div>
+        <div>
+          <img v-if='!agreementList[4]' @click='agreementListHandler(4)' src="@/assets/img/check.png" alt="">
+          <img v-else @click='agreementListHandler(4)' src="@/assets/img/check-square.png" alt="">
+          <p>邮件</p>
+        </div>
+        
+        <div>本人有权通过发送电子邮件[CNacademy@cn.coopervision.com]等方式，随时撤回未来处理本人数据的知情同意。</div>
+        <div>我们收集您的个人信息旨在为您提供与该学习平台有关的服务，包括为您提供访问平台、相关培训和资源的权限。我们可能向参与管理该平台、提供培训、对培训反馈进行数据分析的关联企业和第三方服务提供商披露您的数据，包括位于[库博光学学习中心所在地]、英国和新加坡的企业。若库博光学开展的活动符合持续职业发展（CPD）学时或学分的条件，则我们可能在适当的情况下，将您的个人信息提供给您所在地区负责保管持续职业发展CPD记录的相关机构。若您不同意提供个人信息，我们可能无法为您提供培训和资料。《隐私政策》中解释了库博光学如何收集、使用、处理和披露您的数据，可点击[<a href="https://coopervision.com.cn/privacy-policy">https://coopervision.com.cn/privacy-policy</a>]查看《隐私政策》。《隐私政策》中还详细介绍了您可以如何访问、更正您的个人信息，或者如何对您的个人信息的处理进行投诉。</div>
+        <span @click='goback'>同意并返回注册</span>
       </div>
     </div>
     <!-- 城市 -->
@@ -81,7 +119,8 @@ import { ConstService, UserService } from '@/service'
 export default {
   data() {
     return {
-      isActive: 1,
+      isCheck:false,
+      isActive:1,
       form: {
         name: '',
         // chinese_name:'',
@@ -101,7 +140,8 @@ export default {
       areaList: areaList,
       showArea: false,
       showPosition: false,
-      positionList: []
+      positionList: [],
+      agreementList:[true,true,true,true,true]
     }
   },
   computed: {
@@ -110,13 +150,44 @@ export default {
         return true
       }
       return false
-    }
+    },
   },
   created() {
     this.getPosition()
   },
   methods: {
+    goback(){
+      let temp = this.agreementList.find(i=>{
+        return !i
+      })
+      if(temp === undefined){
+        // 全选
+        this.isCheck = true
+      }else{
+        this.isCheck = false
+      }
+      this.isActive = 2
+    },
+    agreementListHandler(index){
+      let temp = [...this.agreementList]
+      let val = !temp[index]
+      temp.splice(index,1,val)
+      this.agreementList = temp
+    },
+    agreementHandler(){
+      this.isCheck = !this.isCheck
+      if(this.isCheck){
+        this.agreementList = [true,true,true,true,true]
+      }
+    },
+    agreementView(){
+      this.isActive =3
+    },
     register() {
+      if(!this.isCheck){
+        this.$toast('请先阅读并接受《库博用户协议》')
+        return
+      }
       if (!this.form.province) {
         this.$toast('请选择省份')
         return
@@ -202,6 +273,25 @@ export default {
     width: 6.3rem;
     background: rgba(255, 255, 255, 1);
     box-shadow: 0rem 0.06rem 0.4rem 0rem rgba(119, 119, 119, 0.2);
+    .agreement{
+      padding-top: 0.16rem;
+      text-align: center;
+      font-size: 0.2rem;
+      color:#55b6b3;
+      img,span,i{
+        display: inline-block;
+        vertical-align: middle;
+      }
+      i{
+        height: 0.2rem;
+        width: 0.2rem;
+        border: 1px solid red;
+      }
+      img{
+        height: 0.22rem;
+        width: 0.22rem;
+      }
+    }
     >div {
       padding: 0.2rem 0.5rem 0.6rem;
       >ul {
@@ -264,6 +354,31 @@ export default {
         }
       }
     }
+  }
+  .three{
+    >div{
+      position: relative;
+      font-size: 0.24rem;
+      padding-bottom: 0.14rem;
+      line-height: 0.4rem;
+      word-wrap: break-word;
+      word-break: break-all;
+      p{
+        text-indent: 1.3em;
+      }
+      img{
+        position: absolute;
+        top: 0.05rem;
+        height: 0.22rem;
+        width: 0.22rem;
+      } 
+    }
+     span{
+      display: block;
+      text-align: center;
+      font-size: 0.2rem;
+      color:#55b6b3;
+     }
   }
 }
 
